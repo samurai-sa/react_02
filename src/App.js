450f247e4df705bfa94d9b4bd9bd8ba6c8a2be3e
samurai-sa <69831034+samurai-sa@users.noreply.github.com>
2022-08-02 23:36:18 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const LogoutButton = (props) => {
+  <button onClick={props.toggleIsLoggedIn}>ログアウト</button>;
+};
+
+const LoginButton = (props) => {
+  <button onClick={props.toggleIsLoggedIn}>ログイン</button>;
+};
+
+const LoginControl = () => {
+  const [isLoggedIn, setIsLoggedIsState] = useState(false);
+
+  const toggleIsLoggedIn = () => {
+    setIsLoggedIsState(!isLoggedIn);
+  };
+
+  if (isLoggedIn) {
+    return <LogoutButton toggleIsLoggedIn={toggleIsLoggedIn} />;
+  }
+  return <LoginButton toggleIsLoggedIn={toggleIsLoggedIn} />;
+};
+
+export default function App() {
+  return <LoginControl />;
 }
-
-export default App;
